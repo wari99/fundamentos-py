1124ff1e963348172479f12d03ca9304b7cf5a3c
Mariana <57501545+wari99@users.noreply.github.com>
2022-05-19 15:26:58 -0300

    Create l01ex01.py

---
+++ l01ex01.py
@@ -0,0 +1,9 @@
+#Desenvolva um algoritmo que receba quatro números inteiros, calcule e mostre a soma desses números.
+
+n1 = int(input("Primeiro numero: "))
+n2 = int(input("Segundo numero: "))
+n3 = int(input("Terceiro numero: "))
+n4 = int(input("Quarto numero: "))
+
+soma = n1 + n2 + n3 + n4
+print("A soma desses quatro numeros e igual a ", soma)
