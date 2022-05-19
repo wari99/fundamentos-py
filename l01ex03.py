21e2482113b6cbb997384b05a818c725c82cd3a5
Mariana <57501545+wari99@users.noreply.github.com>
2022-05-19 15:49:29 -0300

    Create l01ex03.py

---
+++ l01ex03.py
@@ -0,0 +1,6 @@
+#Faça um algoritmo que receba o salário de um funcionário, calcule e mostre o novo salário, sabendo-se que este sofreu um aumento de 25%.
+
+salariobase = float(input("Salario antigo: R$"))
+salarioatual = salariobase * 1.25
+
+print("O salario com aumento de 25% e: R$", salarioatual)
