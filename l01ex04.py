2182a0feb86814f28f63eb86a7a4b927701e5ce8
Mariana <57501545+wari99@users.noreply.github.com>
2022-05-19 15:55:08 -0300

    Create l01ex04.py

---
+++ l01ex04.py
@@ -0,0 +1,5 @@
+# Faça um algoritmo que responda a área de um círculo dado o raio, fornecido pelo usuário. Use o valor 3,14159 como uma aproximação de π. (S = πr2)
+raio = float(input("Raio do circulo: "))
+areacirculo =  (raio)**2 * 3.14159
+
+print("Area do Circulo: ", areacirculo)
