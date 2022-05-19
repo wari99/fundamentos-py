8a1e26067ce01f1e72589fda12f0fa50ad6a9f65
Mariana <57501545+wari99@users.noreply.github.com>
2022-05-19 15:36:12 -0300

    Create l01ex02.py

---
+++ l01ex02.py
@@ -0,0 +1,12 @@
+#Faça um algoritmo que receba três notas, calcule e mostre a média aritmética entre elas.
+
+nota1 = float(input("1a Nota: "))
+nota2 = float(input("2a Nota: "))
+nota3 = float(input("3a Nota: "))
+
+totalnotas = nota1 + nota2 + nota3
+#print(totalnotas)
+
+medianotas = totalnotas/3
+
+print("Media Aritmetica: ", medianotas)
