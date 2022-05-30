096e463f71793ccff22f758eed1ad966b2acb117
Mariana <57501545+wari99@users.noreply.github.com>
2022-05-30 14:46:58 -0300

    Create l03ex15.py

---
+++ l03ex15.py
@@ -0,0 +1,14 @@
+#27. Faça um algoritmo que leia um número N e verifique se ele é primo.
+
+N = int(input("Numero N: "))
+i = 1
+numdivisores = 0
+
+for i in range(i, N+1):
+  if(N % i == 0):
+    numdivisores += 1
+
+if(numdivisores == 2):
+  print("E primo")
+else:
+  print("Nao e primo")
